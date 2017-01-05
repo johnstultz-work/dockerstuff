cd0e72960bc0690c8ab6431720f558b56ff17944
John Stultz <john.stultz@linaro.org>
2017-01-05 13:33:33 -0800

    build.sh: Make it easier to build with no cache
    Still needs work, but makes it a bit easier to enable
    building images w/o cache
    
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- build.sh
+++ build.sh
@@ -3,12 +3,15 @@
 USER=`whoami`
 LIST=`ls Dockerfile* | sed -e 's/Dockerfile.//'`
 
-if [-n $1]
+#NO_CACHE="--no-cache"
+NO_CACHE=""
+
+if [ "$1" != "" ]
 then
 	LIST=$1
 fi
 
 for i in $LIST
 do
-	docker build -t $USER/$i . -f Dockerfile.$i
+	docker build $NO_CACHE -t $USER/$i . -f Dockerfile.$i
 done
