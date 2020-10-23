2f4b55b2988988173e9027239942367160224b0c
John Stultz <john.stultz@linaro.org>
2020-10-23 05:30:33 +0000

    Add updated cts and vts docker files
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- cmds/docker-cts.sh
+++ cmds/docker-cts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-docker run -i -t --privileged -v /dev/bus/usb:/dev/bus/usb -v $1:/home/docker/persist jstultz/cts bin/bash
+ARGS="${@:2}"
+docker run -i -t --privileged -v /dev/bus/usb:/dev/bus/usb -e ARGS="$ARGS" -v $1:/home/docker/persist jstultz/cts
 
---
+++ cmds/docker-vts.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ARGS="${@:2}"
+docker run -i -t --privileged -v /dev/bus/usb:/dev/bus/usb -e ARGS="$ARGS" -v $1:/home/docker/persist jstultz/vts
+
